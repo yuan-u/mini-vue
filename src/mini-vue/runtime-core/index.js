7b7911f21b91858b3af72817fdc1a4ada8035756
yuan-u <1475595187@qq.com>
2023-03-30 18:03:53 +0800

    patch

--- src/mini-vue/runtime-core/index.js
+++ src/mini-vue/runtime-core/index.js
@@ -9,7 +9,9 @@ export function createRenderer(options) {
     //render负责渲染组件内容
     const {
         createElement:hostCreateElement,
-        insert:hostInsert
+        insert:hostInsert,
+        setElementText: hostSetElementText,
+        remove: hostRemove
     }= options;
 
     //无虚拟dom版本
@@ -90,18 +92,27 @@ export function createRenderer(options) {
             if (!instance.isMounted) {
                 //创建阶段
                 //执行组件渲染函数获取vnode
-                // const {render} = instance.vnode.type
-                const vnode= (instance.subtree = render.call(instance.data));
+                // const {render} = instance.vnode.type 
+                const vnode= (instance.subtree = render.call(instance.data)); //保存最新的vnode
                 //递归patch嵌套节点
                 patch(null,vnode,container)
 
                 //挂载钩子
-                if (instance.vnode.type.isMounted) {
-                    instance.vnode.type.isMounted.call(instance.data)
+                if (instance.vnode.type.mounted) {
+                    instance.vnode.type.mounted.call(instance.data)
                 }
                 instance.isMounted = true;
             }else{
-                //
+                // 更新阶段
+                const prevVnode = instance.subtree;
+                // 获取最新的VNode
+                const nextVnode = render.call(instance.data);
+                // 保存下次更新使用
+                instance.subtree = nextVnode;
+                // 执行patch并传入新旧两个Vnode
+                patch(prevVnode, nextVnode);
+
+
             }
         }
         //建立更新机制
@@ -132,6 +143,58 @@ export function createRenderer(options) {
         //插入元素
         hostInsert(el,container)
     }
+
+    const patchElement = (n1, n2) => {
+        // 获取要更新的元素节点
+        const el = (n2.el = n1.el);
+
+        // 更新type相同的节点，实际上还要考虑key
+        if (n1.type === n2.type) {
+        // 获取双方子元素
+        const oldCh = n1.children;
+        const newCh = n2.children;
+
+        // 根据双方子元素情况做不同处理
+        if (typeof oldCh === "string") {
+            if (typeof newCh === "string") {
+            // 对比双方文本，如果变化则更新
+            if (oldCh !== newCh) {
+                hostSetElementText(el, newCh);
+            }
+            } else {
+            // 替换文本为一组子元素
+            hostSetElementText(el, "");
+            newCh.forEach((v) => patch(null, v, el));
+            }
+        } else {
+            if (typeof newCh === "string") {
+            // 之前是子元素数组，变化之后是文本内容
+            hostSetElementText(el, newCh);
+            } else {
+            // 变化前后都是子元素数组
+            updateChildren(oldCh, newCh, el);
+            }
+        }
+        }
+    };
+    const updateChildren = (oldCh, newCh, parentElm) => {
+        // A B C D E
+        // A C D E
+        // 获取较短的数组的长度
+        const len = Math.min(oldCh.length, newCh.length);
+        for (let i = 0; i < len; i++) {
+        patch(oldCh[i], newCh[i]);
+        }
+        // 获取较长数组中剩余的部分
+        if (newCh.length > oldCh.length) {
+        // 新数组较长，剩余的批量创建追加
+        newCh.slice(len).forEach((child) => patch(null, child, parentElm));
+        } else {
+        // 老数组较长，剩余的批量删除
+        oldCh.slice(len).forEach(child => hostRemove(child.el))
+        }
+    };
+
     //返回一个渲染器实例
     return{
         render,
